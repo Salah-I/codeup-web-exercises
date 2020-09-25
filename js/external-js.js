a55b278824d571dfc2162dd9f23fd85ee850e6df
Salah Ismail <salah.o.ismail01@gmail.com>
2020-09-25 04:23:40 -0500

    working on exercises

--- js/external-js.js
+++ js/external-js.js
@@ -4,3 +4,13 @@ console.log("this is the external js file");
 
 var myAge = 50;
 console.log(myAge);
+var google = 400 * 6
+var amazon = 380 * 4
+var facebook 350 * 10
+facebook + amazon + google
+
+
+var classMax = x
+var currentnumberostudents = y
+var classopen = (x>y)
+
