13ab70d6aa260b2d7c070c6d474bae33c02cedb0
Salah Ismail <salah.o.ismail01@gmail.com>
2020-09-28 15:18:13 -0500

    functions.js

--- js/external_js.js
+++ js/external_js.js
@@ -1,1 +1,28 @@
 "use script"
+
+alert('Welcome to my website')
+console.log(alert)
+
+var userInput = prompt('whats your fav color')
+prompt('whats your fav color')
+console.log('the user entered' + userInput)
+
+var cost = (days * 3)
+var days = (numberofdays)
+var numberofdays = prompt('how many days you rent the movie')
+console.log(cost)
+
+console.log("this is the external js file");
+
+
+var google = prompt(400 * 6)
+var amazon = prompt(380 * 4)
+var facebook = prompt(350 * 10)
+facebook + amazon + google
+
+
+var classMax = x
+var currentnumberofstudents = y
+var classopen = (x>y)
+
+var confirm = confirm('is the class full')
